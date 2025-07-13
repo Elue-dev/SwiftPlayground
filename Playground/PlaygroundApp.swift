c7c8976b12f6eb736a7d0ded338d4f02586687be
Wisdom Elue <eluewisdom@gmail.com>
2025-07-13 16:07:00 +0100

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Wisdom on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
